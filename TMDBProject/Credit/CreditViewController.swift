8ccbbd51478c48d6b827794339f75b9cd5d700c1
knockknock <yangban11111@gmail.com>
2024-06-30 23:40:33 +0900

    상단 포스터 이미지 위치 설정

--- TMDBProject/Credit/CreditViewController.swift
+++ TMDBProject/Credit/CreditViewController.swift
@@ -10,6 +10,12 @@ import SnapKit
 
 class CreditViewController: BaseViewController {
     
+    private let posterImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .brown
+        return imageView
+    }()
+    
     private let movieTitleLabel = {
         let label = UILabel()
         label.textAlignment = .left
@@ -24,11 +30,15 @@ class CreditViewController: BaseViewController {
     }
     
     override func configureHierarchy() {
-        [movieTitleLabel].forEach {
+        [posterImageView, movieTitleLabel].forEach {
             view.addSubview($0)
         }
     }
     override func configureLayout() {
+        posterImageView.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(view.safeAreaLayoutGuide).multipliedBy(0.25)
+        }
         movieTitleLabel.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
