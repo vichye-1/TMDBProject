085488ad000501ba4f87d90724449f178ebfe610
knockknock <yangban11111@gmail.com>
2024-06-30 23:19:08 +0900

    creditVC movieTitleLabel 위치 설정

--- TMDBProject/Credit/CreditViewController.swift
+++ TMDBProject/Credit/CreditViewController.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+import SnapKit
 
 class CreditViewController: BaseViewController {
+    
+    private let movieTitleLabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.font = Constant.Font.bold17
+        label.textColor = .white
+        label.backgroundColor = .systemCyan
+        return label
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    
+    override func configureHierarchy() {
+        [movieTitleLabel].forEach {
+            view.addSubview($0)
+        }
+    }
+    override func configureLayout() {
+        movieTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.height.equalTo(34)
+        }
+    }
 }
